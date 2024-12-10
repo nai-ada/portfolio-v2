576c6dba351a9e7f1011e88a6a3dea125f8fdbbd
Nadia <naiya.ada003@gmail.com>
2024-12-10 00:57:43 -0800

    update on custom loader bug

--- src/components/CustomLoader.jsx
+++ src/components/CustomLoader.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import loaderSvg from '../assets/loader.svg'
 
 export default function CustomLoader() {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-darkBackground z-50">
-            <img src="/src/assets/loader.svg" className="w-16 h-16" alt="Loading..." />
+            <img src={loaderSvg} className="w-16 h-16" alt="Loading..." />
         </div>
     );
 }
